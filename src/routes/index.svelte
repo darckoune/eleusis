e4b24be2c1d66a28e27d2c9fd2723dbe19f5e730
Darckoune <darckoune@gmail.com>
2019-06-20 00:00:58 +0200

    main window

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,28 @@
-<p>Hello Eleusis !</p>
+<script>
+  import Timeline from "../components/Timeline.svelte";
+</script>
+
+<style>
+  .page-container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .main {
+    flex: 1;
+    border-bottom: 1px solid black;
+  }
+
+  footer {
+    height: 200px;
+  }
+</style>
+
+<div class="page-container">
+  <div class="main">
+    <Timeline/>
+  </div>
+
+  <footer />
+</div>
