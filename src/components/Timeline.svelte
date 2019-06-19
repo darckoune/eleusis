71cfd03040e38dd3da19bd14ce3270e6f7e6214a
Darckoune <darckoune@gmail.com>
2019-06-20 00:01:20 +0200

    Timeline with horizontal scroll

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Card from "./Card.svelte";
+  function scrollHorizontally(e) {
+    var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    e.target.scrollLeft -= delta * 40;
+    e.preventDefault();
+  }
+</script>
+
+<style>
+  .full {
+    height: 100%;
+    width: 100%;
+    overflow-x: auto;
+    background-color: #f0f0f0;
+  }
+</style>
+
+<div
+  class="full"
+  on:mousewheel={scrollHorizontally}
+  on:DOMMouseScroll={scrollHorizontally}>
+  <Card />
+</div>
