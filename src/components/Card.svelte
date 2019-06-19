3b1e818239bbf2b35426925bcf07b889fa4a759b
Darckoune <darckoune@gmail.com>
2019-06-20 00:01:28 +0200

    basic card

---
+++ src/components/Card.svelte
@@ -0,0 +1,13 @@
+<style>
+  .card {
+    width: 172px;
+    height: 240px;
+    display: inline-block;
+    border-radius: 5%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.18);
+  }
+</style>
+
+<div class="card">
+  
+</div>
